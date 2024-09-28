51d04e24031f4593649bac348719660187278d05
Emirhan Çalarağlar <emirhancalaraglar@gmail.com>
2024-09-28 15:09:36 +0300

    01_MainSubjects

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Merhaba Dünya");
+            Console.Read();
+        }
+    }
+}
+
+
+// Yazdırma komutları
